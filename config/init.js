68e24e2bac20ea468a44c20c071f5b5d615d574d
Peter Deal <pdeal@nl.rogers.com>
2016-10-07 12:02:54 -0230

    update globs to use glob.sync rather than deprecated option sync

--- config/init.js
+++ config/init.js
@@ -14,20 +14,17 @@ module.exports = function() {
 	 * Before we begin, lets set the environment variable
 	 * We'll Look for a valid NODE_ENV variable and if one cannot be found load the development NODE_ENV
 	 */
-	glob('./config/env/' + process.env.NODE_ENV + '.js', {
-		sync: true
-	}, function(err, environmentFiles) {
-		if (!environmentFiles.length) {
-			if (process.env.NODE_ENV) {
-				console.error(chalk.red('No configuration file found for "' + process.env.NODE_ENV + '" environment using development instead'));
-			} else {
-				console.error(chalk.red('NODE_ENV is not defined! Using default development environment'));
-			}
+	var environmentFiles = glob.sync('./config/env/' + process.env.NODE_ENV + '.js');
 
-			process.env.NODE_ENV = 'development';
+	if (!environmentFiles.length) {
+		if (process.env.NODE_ENV) {
+			console.error(chalk.red('No configuration file found for "' + process.env.NODE_ENV + '" environment using development instead'));
 		} else {
-			console.log(chalk.black.bgWhite('Application loaded using the "' + process.env.NODE_ENV + '" environment configuration'));
+			console.error(chalk.red('NODE_ENV is not defined! Using default development environment'));
 		}
-	});
 
+		process.env.NODE_ENV = 'development';
+	} else {
+		console.log(chalk.black.bgWhite('Application loaded using the "' + process.env.NODE_ENV + '" environment configuration'));
+	}
 };
