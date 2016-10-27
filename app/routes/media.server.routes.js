b46c66dca281d8a8d964eb1277d590fe6a8a7977
Peter Deal <pdeal@nl.rogers.com>
2016-10-27 17:09:53 -0230

    finish logo upload dialog

--- app/routes/media.server.routes.js
+++ app/routes/media.server.routes.js
@@ -6,19 +6,20 @@
 var media = require('../controllers/media.server.controller'),
     multer = require('multer');
 
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/media');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+
+var upload = multer({ storage: storage })
 module.exports = function(app) {
 	// Media routes
 	app.route('/api/media/uploadLogo')
-		.post(multer( {
-			dest: "public/media",
-			renameFile: (fieldname, filename) => {
-				console.log("fieldname is " + fieldname);
-				console.log("filename is " + filename);
-			},
-			onFileUploadComplete: () => {
-				console.log({ text: "hey, it's complete" });
-			}
-		}).single('file'), media.uploadLogo);
+		.post(multer({storage:storage}).single('file'), media.uploadLogo);
 
 	app.route('/api/media')
 		.post(media.save);
