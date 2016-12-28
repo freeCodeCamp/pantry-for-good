3530b0cbdc7c445cbfae367a30a75beb3e41f7ce
Marco Fruh <marco.fruh@me.com>
2016-12-28 15:13:34 +0100

    q editor: client views cleanup

--- public/modules/customer/config/user-customer.client.routes.js
+++ public/modules/customer/config/user-customer.client.routes.js
@@ -12,17 +12,8 @@ angular.module('customer').config(['$stateProvider',
 					templateUrl: 'modules/customer/views/user/create-customer.client.view.html',
 					controller: 'CustomerUserController as customerCtrl'
 				},
-				'general-info@root.createCustomerUser': {
-					templateUrl: 'modules/customer/views/partials/general-info.partial.html'
-				},
-				'employment@root.createCustomerUser': {
-					templateUrl: 'modules/customer/views/partials/employment.partial.html'
-				},
 				'food-preferences@root.createCustomerUser': {
 					templateUrl: 'modules/customer/views/partials/food-preferences.partial.html'
-				},
-				'financial@root.createCustomerUser': {
-					templateUrl: 'modules/customer/views/partials/financial.partial.html'
 				},
 				'household@root.createCustomerUser': {
 					templateUrl: 'modules/customer/views/partials/household.partial.html'
@@ -56,17 +47,8 @@ angular.module('customer').config(['$stateProvider',
 					templateUrl: 'modules/customer/views/edit-customer.client.view.html',
 					controller: 'CustomerUserController as customerCtrl'
 				},
-				'general-info@root.editCustomerUser': {
-					templateUrl: 'modules/customer/views/partials/general-info.partial.html'
-				},
-				'employment@root.editCustomerUser': {
-					templateUrl: 'modules/customer/views/partials/employment.partial.html'
-				},
 				'food-preferences@root.editCustomerUser': {
 					templateUrl: 'modules/customer/views/partials/food-preferences.partial.html'
-				},
-				'financial@root.editCustomerUser': {
-					templateUrl: 'modules/customer/views/partials/financial.partial.html'
 				},
 				'household@root.editCustomerUser': {
 					templateUrl: 'modules/customer/views/partials/household.partial.html'
