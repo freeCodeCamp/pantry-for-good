15b65d78464130a8cb436732aa81d849bcede267
Nick Hu <nick@nickhu.info>
2017-01-13 15:36:56 +0800

    volunteer creation redirects to edit page on create

--- public/modules/volunteer/controllers/admin-volunteer.client.controller.js
+++ public/modules/volunteer/controllers/admin-volunteer.client.controller.js
@@ -4,7 +4,7 @@
 	angular.module('volunteer').controller('VolunteerAdminController', VolunteerAdminController);
 
 	/* @ngInject */
-	function VolunteerAdminController($window, $stateParams, $state, Authentication, VolunteerAdmin, VolunteerUser) {
+	function VolunteerAdminController($window, $stateParams, $state, Authentication, VolunteerAdmin, VolunteerUser, Section, Field, $q) {
 		var self = this;
 
 		// This provides Authentication context
@@ -22,9 +22,84 @@
 			}
 		};
 
+		self.handleCheckboxClick = function (name, element) {
+
+			// Initialise as array if undefined
+			if (typeof self.volunteer[name] !== 'object') {
+				self.volunteer[name] = [element];
+				return;
+			}
+
+			// If element is not yet in array, push it, otherwise delete it from array
+			var i = self.volunteer[name].indexOf(element);
+			if (i === -1) {
+				self.volunteer[name].push(element);
+			} else {
+				self.volunteer[name].splice(i, 1);
+			}
+		};
+
+		self.generateForm = function() {
+			var cRows = _.maxBy(self.fields, 'row').row, cCols = 4;
+			var r, cSkip = 0;
+			var emptyCell = { status: 'empty' };
+			var skipCell = { status: 'skip' };
+
+			var dynamicForm = [];
+
+			// Limit to available sections of volunteer Questionnaire
+			self.filteredSections = _.sortBy(_.filter(self.sections, {'questionnaire': { 'identifier': 'qVolunteers' }}), 'position');
+
+			for (var s = 0; s < self.filteredSections.length; s++) {
+				var tmpRow = [];
+				for (var i = 0; i < cRows; i++) {
+					var tmpArr = [];
+					for (var j = 0; j < cCols; j++) {
+						if (cSkip > 0) {
+							tmpArr.push(skipCell);
+							cSkip--;
+						} else {
+							r = _.find(self.fields, {
+								'row': i + 1,
+								'column': j + 1,
+								'section': self.filteredSections[s]
+							});
+
+							if (r === undefined) {
+								tmpArr.push(emptyCell);
+							} else {
+								r.status = 'valid';
+								tmpArr.push(r);
+								cSkip = r.span - 1;
+							} // if r is undefined
+						} // if skip cells left
+
+					} // for j, cells
+					tmpRow.push(tmpArr);
+				} // for i, rows
+				dynamicForm.push(tmpRow);
+			} // for s, sections
+
+			self.dynForm = dynamicForm;
+		}; // Function generate Form
+
+		var promiseHash = {};
+		promiseHash.sections = Section.query().$promise;
+		promiseHash.fields = Field.query().$promise;
+
+		$q.all(promiseHash)
+			.then(function(results) {
+				self.questionnaires = results.questionnaires;
+				self.sections = results.sections;
+				self.fields = results.fields;
+
+				self.generateForm();
+		});
+
+
 		// Find a list of volunteers
 		self.find = function() {
-			self.volunteer = VolunteerAdmin.query();
+			self.volunteers = VolunteerAdmin.query();
 		};
 
 		// Find existing volunteer
@@ -38,20 +113,17 @@
 
 		// Create a new volunteer
 		self.createNewVolunteer = function() {
-			console.log('new volunteer')
 			var volunteer = new VolunteerUser({
 				lastName: "test",
 				firstName: "testing",
 				email: 'test@test.com',
 				manualAdd: true
 			});
-
-			volunteer.$save(function(u, putResponseHeaders) {
-			console.log('returned data',u._id);
-			alert('pause')
-			console.log(putResponseHeaders);
+			volunteer.$save(function(response) {
+				// Redirect after save
+				$state.go('root.editVolunteerAdmin', { volunteerId: response._id });
 			}, function(errorResponse) {
-				self.errorItem = errorResponse.data.message;
+				self.error = errorResponse.data.message;
 			});
 		};
 
