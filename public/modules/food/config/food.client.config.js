40af2e1cd147019c1c972587fcd894d77b7bec35
MAF <marco.fruh@me.com>
2017-02-11 11:29:56 +0100

    Fixed routing for food inventory.

--- public/modules/food/config/food.client.config.js
+++ public/modules/food/config/food.client.config.js
@@ -4,6 +4,6 @@
 angular.module('food').run(['Menus',
 	function(Menus) {
 		// Set sidebar menu items for admin
-		Menus.addMenuItem('admin', 'Inventory', 'admin/foods', 'item', 'root.foods', '', ['admin'], 3);
+		Menus.addMenuItem('admin', 'Inventory', 'admin/foods', 'item', 'root.foodsAdmin', '', ['admin'], 3);
 	}
 ]);
