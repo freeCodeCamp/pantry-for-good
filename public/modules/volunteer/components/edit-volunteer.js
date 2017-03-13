e07fbc8ec9185c8318e877d43ec9fe611c64bf44
jspaine <painejs@hotmail.com>
2017-03-13 15:03:34 +0100

    add missed volunteer edit component

--- public/modules/volunteer/components/edit-volunteer.js
+++ public/modules/volunteer/components/edit-volunteer.js
@@ -1,11 +1,97 @@
 import angular from 'angular';
+import {stateGo} from 'redux-ui-router';
+
+import {selectors} from '../../../store';
+import {loadVolunteer, saveVolunteer} from '../../../store/volunteer';
+import {loadFields} from '../../../store/field';
+import {loadFoods} from '../../../store/food-category';
+import {loadSections} from '../../../store/section';
+
+const mapStateToThis = state => ({
+  user: state.auth.user,
+  getVolunteer: selectors.getOneVolunteer(state),
+  loadingVolunteers: selectors.loadingVolunteers(state),
+  loadVolunteersError: selectors.loadVolunteersError(state),
+  savingVolunteers: selectors.savingVolunteers(state),
+  saveVolunteersError: selectors.saveVolunteersError(state),
+  formData: selectors.getFormData(state),
+  loadingFormData: selectors.loadingFormData(state),
+  loadFormDataError: selectors.loadFormDataError(state),
+  volunteerId: state.router.currentParams.volunteerId,
+  settings: state.settings.data,
+});
+
+const mapDispatchToThis = dispatch => ({
+  loadVolunteer: (id, admin) => dispatch(loadVolunteer(id, admin)),
+  _saveVolunteer: (volunteer, admin) => dispatch(saveVolunteer(volunteer, admin)),
+  loadFormData: () => {
+    dispatch(loadFoods());
+    dispatch(loadFields());
+    dispatch(loadSections());
+  },
+  push: (route, params, options) => dispatch(stateGo(route, params, options))
+});
 
 export default angular.module('volunteer')
   .component('editVolunteer', {
-    controller: 'VolunteerController',
+    controller: function($ngRedux, Form) {
+      this.$onInit = () => {
+        this.unsubscribe = $ngRedux.connect(mapStateToThis, mapDispatchToThis)(this);
+        this.prevState = {};
+        this.loadedFormData = false;
+        this.initialized = false;
+
+        this.isAdmin = this.user.roles.find(role => role === 'admin');
+        this.formMethods = Form.methods;
+
+        this.loadVolunteer(this.volunteerId, this.isAdmin);
+        this.loadFormData();
+      };
+
+      this.$doCheck = () => {
+        // Tried to save volunteer
+        if (this.prevState.savingVolunteers && !this.savingVolunteers) {
+          if (this.savingVolunteersError) this.error = this.savingVolunteersError;
+          else this.push(this.isAdmin ? 'root.listVolunteers' : 'root');
+        }
+
+        // Tried to load volunteer
+        if (this.prevState.loadingVolunteers && !this.loadingVolunteers) {
+          if (this.loadVolunteersError) this.error = this.loadVolunteersError;
+          else this.volunteer = this.getVolunteer(this.volunteerId);
+        }
+
+        // Tried to load form data
+        if (this.prevState.loadingFormData && !this.loadingFormData) {
+          if (this.loadFormDataError) this.error = this.loadFormDataError;
+          else this.loadedFormData = true
+        }
+
+        // Set up form if data loaded and not already done
+        if (!this.initialized && this.loadedFormData && this.volunteer) {
+          console.log('this.volunteer.dateOfBirth', this.volunteer.dateOfBirth)
+
+          this.volunteerModel = {
+            ...this.volunteer,
+            dateOfBirth: new Date(this.volunteer.dateOfBirth)
+          };
+
+          this.volunteerForm = this.formMethods.generate(this.volunteerModel,
+                                      this.formData, 'qVolunteers');
+
+          this.initialized = true;
+        }
+
+        this.prevState = {...this};
+      };
+
+      this.$onDestroy = () => this.unsubscribe();
+
+      this.saveVolunteer = () => this._saveVolunteer(this.volunteer, this.isAdmin);
+    },
     template: `
       <!-- Content header (Page header) -->
-      <section class="content-header" data-ng-init="$ctrl.findOne()">
+      <section class="content-header">
         <h1>{{$ctrl.volunteer.firstName}} {{$ctrl.volunteer.lastName}}</h1>
       </section>
       <!-- Main content -->
@@ -13,17 +99,17 @@ export default angular.module('volunteer')
         <div class="row">
           <div class="col-xs-12">
             <!-- Form -->
-            <form name="volunteerForm" data-ng-submit="volunteerForm.$valid && $ctrl.update()">
+            <form name="volunteerForm" data-ng-submit="volunteerForm.$valid && $ctrl.saveVolunteer()">
               <!-- Identification and General Information -->
               <dynamic-form
-                section-names="$ctrl.sectionNames"
-                dyn-form="$ctrl.dynForm"
-                dyn-type="$ctrl.dynType"
-                food-list="$ctrl.foodList"
-                is-checked="$ctrl.dynMethods.isChecked(dynType, cellName, choice)"
-                handle-checkbox="$ctrl.dynMethods.handleCheckbox(dynType, cellName, choice)"
-                food-is-checked="$ctrl.dynMethods.foodIsChecked(dynType, food)"
-                toggle-food-selection="$ctrl.dynMethods.toggleFoodSelection(dynType, food)"
+                section-names="$ctrl.volunteerForm.sections"
+                dyn-form="$ctrl.volunteerForm.dynForm"
+                dyn-type="$ctrl.volunteerModel"
+                food-list="$ctrl.volunteerForm.foods"
+                is-checked="$ctrl.formMethods.isChecked(dynType, cellName, choice)"
+                handle-checkbox="$ctrl.formMethods.handleCheckbox(dynType, cellName, choice)"
+                food-is-checked="$ctrl.formMethods.foodIsChecked(dynType, food)"
+                toggle-food-selection="$ctrl.formMethods.toggleFoodSelection(dynType, food)"
               />
               <!-- Buttons -->
               <div class="row">
@@ -31,8 +117,8 @@ export default angular.module('volunteer')
                   <button type="submit" class="btn btn-success btn-block top-buffer">Update</button>
                 </div>
                 <div class="col-sm-6 col-md-4 col-lg-2">
-                  <a class="btn btn-primary btn-block top-buffer" data-ng-if="$ctrl.auth.user.roles.indexOf('admin') >= 0" data-ng-href="/#!/admin/volunteers">Cancel</a>
-                  <a class="btn btn-primary btn-block top-buffer" data-ng-if="$ctrl.auth.user.roles.indexOf('admin') < 0" data-ng-href="/#!/">Cancel</a>
+                  <a class="btn btn-primary btn-block top-buffer" data-ng-if="$ctrl.isAdmin" data-ng-href="/#!/admin/volunteers">Cancel</a>
+                  <a class="btn btn-primary btn-block top-buffer" data-ng-if="!$ctrl.isAdmin" data-ng-href="/#!/">Cancel</a>
                 </div>
               </div><!-- /.buttons -->
               <!-- Error -->
