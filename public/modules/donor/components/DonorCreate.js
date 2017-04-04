b63e4f75c1e97b5b1715326019b3055e2bdc8bb6
jspaine <painejs@hotmail.com>
2017-04-05 00:13:10 +0200

    convert donor and donation components to react

---
+++ public/modules/donor/components/DonorCreate.js
@@ -0,0 +1,148 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {stateGo} from 'redux-ui-router'
+import set from 'lodash/set'
+
+import {Form} from '../../common/services/form'
+import {selectors} from '../../../store';
+import {saveDonor} from '../../../store/donor';
+import {loadFields} from '../../../store/field';
+import {loadFoods} from '../../../store/food-category';
+import {loadSections} from '../../../store/section';
+
+import Page from '../../common/components/Page'
+import DynamicForm from '../../common/components/DynamicForm'
+import FoodbankLogo from '../../media/components/foodbank-logo'
+
+const mapStateToProps = state => ({
+  user: state.auth.user,
+  savingDonors: selectors.savingDonors(state),
+  saveDonorsError: selectors.saveDonorsError(state),
+  formData: selectors.getFormData(state),
+  loadingFormData: selectors.loadingFormData(state),
+  loadFormDataError: selectors.loadFormDataError(state),
+  settings: state.settings.data,
+});
+
+const mapDispatchToProps = dispatch => ({
+  saveDonor: donor => dispatch(saveDonor(donor)),
+  loadFormData: () => {
+    dispatch(loadFoods());
+    dispatch(loadFields());
+    dispatch(loadSections());
+  },
+  push: (route, params, options) => dispatch(stateGo(route, params, options))
+});
+
+class DonorCreate extends Component {
+  constructor(props) {
+    super(props)
+    this.isAdmin = props.user.roles.find(role => role === 'admin')
+    this.formMethods = Form.methods
+    this.state = {
+      donorModel: null,
+      donorForm: null,
+      error: null,
+    }
+  }
+
+  componentWillMount() {
+    this.props.loadFormData();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const {
+      savingDonors,
+      saveDonorsError,
+      loadingFormData,
+      loadFormDataError,
+      getDonor,
+      formData
+    } = nextProps
+
+    // Tried to save donor
+    if (!savingDonors && this.props.savingDonors) {
+      if (saveDonorsError) this.setState({error: saveDonorsError})
+      else this.props.push(this.isAdmin ? 'root.listDonors' : 'root')
+    }
+
+    // Tried to load form data
+    if (!loadingFormData && this.props.loadingFormData) {
+      if (loadFormDataError) this.setState({error: loadFormDataError})
+      else this.formData = formData
+    }
+
+    // Set up form if data loaded and not already done
+    if (this.formData && !this.state.donorForm) {
+      const donorModel = {...this.props.user}
+      const donorForm = this.formMethods.generate(donorModel,
+                                  this.formData, 'qDonors')
+
+      this.setState({donorModel, donorForm})
+    }
+  }
+
+  saveDonor = ev => {
+    ev.preventDefault()
+    this.props.saveDonor(this.state.donorModel, this.isAdmin)
+  }
+
+  handleFieldChange = (field, value) => ev => {
+    if (!value) value = ev.target.value
+    let items
+
+    if (ev.target.type === 'checkbox')
+      items = this.formMethods.toggleCheckbox(this.state.donorModel, field, value)
+
+    const donorModel = set({...this.state.donorModel}, field, items || value)
+    this.setState({donorModel})
+  }
+
+  render() {
+    const {donorModel, donorForm, error} = this.state
+    const {settings} = this.props
+    if (!donorModel || !donorForm) return null
+    return (
+      <div>
+        <section className="content-header text-center">
+          <FoodbankLogo />
+          <h1>Donor Profile Creation</h1>
+          <br />
+          <div className="alert alert-info text-left">
+            <i className="icon fa fa-warning"></i>For assistance with this application, please contact our support line at
+            {settings.supportNumber}.
+          </div>
+        </section>
+        <section className="content">
+          <div className="row">
+            <div className="col-xs-12">
+              <form name="donorForm" onSubmit={this.saveDonor}>
+                <DynamicForm
+                  sectionNames={donorForm.sectionNames}
+                  dynForm={donorForm.dynForm}
+                  dynType={donorModel}
+                  handleFieldChange={this.handleFieldChange}
+                />
+                <div className="row">
+                  <div className="col-sm-6 col-md-4 col-lg-2">
+                    <button type="submit" className="btn btn-success btn-block top-buffer">Submit</button>
+                  </div>
+                  <div className="col-sm-6 col-md-4 col-lg-2">
+                    <a className="btn btn-primary btn-block top-buffer" href="/#!/">Cancel</a>
+                  </div>
+                </div>
+                {error &&
+                  <div className="text-danger">
+                    <strong>{error}</strong>
+                  </div>
+                }
+              </form>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DonorCreate)
--- public/modules/donor/components/donor-view.js
+++ public/modules/donor/components/donor-view.js
@@ -1,148 +1,34 @@
+import React from 'react'
+import {Provider, connect} from 'react-redux'
+import ReactDOM from 'react-dom'
+import {AppContainer} from 'react-hot-loader'
 import angular from 'angular';
-import {stateGo} from 'redux-ui-router';
 
-import {selectors} from '../../../store';
-import {loadDonor, saveDonor, deleteDonor} from '../../../store/donor';
+import DonorView from './DonorView'
 
-const mapStateToThis = state => ({
-  user: state.auth.user,
-  savingDonor: state.donor.saving,
-  savingDonorError: state.donor.saveError,
-  loadingDonors: selectors.loadingDonors(state),
-  loadDonorsError: selectors.loadDonorsError(state),
-  getDonor: selectors.getOneDonor(state),
-  donorId: state.router.currentParams.donorId,
-  settings: state.settings.data
-});
-
-const mapDispatchToThis = dispatch => ({
-  loadDonor: (id, admin) => dispatch(loadDonor(id, admin)),
-  deleteDonor: id => dispatch(deleteDonor(id)),
-  push: (route, params, options) => dispatch(stateGo(route, params, options))
-});
-
-
-export default angular.module('donor')
+export default angular.module('customer')
   .component('donorView', {
-    controller: function($ngRedux, $uibModal) {
-      this.$onInit = () => {
-        this.unsubscribe = $ngRedux.connect(mapStateToThis, mapDispatchToThis)(this);
-        this.prevState = {};
-        this.isAdmin = this.user.roles.find(role => role === 'admin');
-        this.loadDonor(this.donorId, this.isAdmin);
-      };
+    controller: function($ngRedux) {
+      render(DonorView)
 
-      this.$doCheck = () => {
-        // Tried to save donor
-        if (!this.savingDonors && this.prevState.savingDonors) {
-          if (this.savingDonorError) this.error = this.savingDonorError;
-          else this.donor = this.getDonor(this.donorId);
-          console.log('this.donor', this.donor)
-
-        }
-
-        // Tried to load donor
-        if (!this.loadingDonors && this.prevState.loadingDonors) {
-          if (this.loadDonorsError) {
-            this.error = this.loadDonorsError;
-          } else {
-            this.donor = this.getDonor(this.donorId);
-            this.donationsModel = [...this.donor.donations];
-          }
-        }
-
-        this.prevState = {...this};
+      function render(Component) {
+        ReactDOM.render(
+          <AppContainer>
+            <Provider store={$ngRedux}>
+              <Component />
+            </Provider>
+          </AppContainer>,
+          document.getElementById('donor-view')
+        )
       }
 
-      this.$onDestroy = () => this.unsubscribe();
-
-      // Open donation form in a modal window
-      this.newDonation = () => {
-        var modalInstance = $uibModal.open({
-          component: 'donationCreate',
-          resolve: {
-            donor: () => this.donor
-          }
-        });
-      };
-
-      // View donation in a modal window
-      this.viewDonation = donation => {
-        $uibModal.open({
-          component: 'donationView',
-          resolve: {
-            donation: () => donation,
-            donor: () => this.donor,
-            settings: () => this.settings
-          }
-        });
-      };
+      if (module.hot) {
+        module.hot.accept('./DonorView', () => {
+          const Next = require('./DonorView').default
+          render(Next)
+        })
+      }
     },
-    template: `
-      <!-- Content header (Page header) -->
-      <section class="content-header">
-        <h1><span data-ng-bind="$ctrl.donor.fullName"></span></h1>
-      </section>
-      <!-- Main content -->
-      <section class="content">
-        <div class="row">
-          <div class="col-xs-12">
-            <!-- Box -->
-            <div class="box">
-              <!-- Box header -->
-              <div class="box-header">
-                <h3 class="box-title">Donations</h3>
-                <div class="box-tools">
-                  <button class="btn btn-success btn-flat" data-ng-click="$ctrl.newDonation()">
-                    <i class="fa fa-plus"></i> Add Donation
-                  </button>
-                </div>
-              </div><!-- /.box-header-->
-              <!-- Box body -->
-              <div class="box-body table-responsive no-padding top-buffer">
-                <!-- Table -->
-                <table class="table table-bordered table-striped" st-table="$ctrl.donationsModel" st-safe-src="$ctrl.donor.donations" print-section>
-                  <!-- Table columns -->
-                  <thead>
-                    <tr>
-                      <th st-sort="dateReceived">Date</th>
-                      <th st-sort="eligibleForTax">Amount</th>
-                      <th st-sort="type">Type</th>
-                      <th st-sort="_id">Tax receipt ID</th>
-                      <th print-remove>Actions</th>
-                    </tr>
-                  </thead><!-- /.table-columns -->
-                  <!-- Table body -->
-                  <tbody>
-                    <tr data-ng-repeat="donation in $ctrl.donor.donations">
-                      <td><span data-ng-bind="donation.dateReceived | date:'shortDate'"></span></td>
-                      <td><span data-ng-bind="donation.eligibleForTax | currency:'$':2"></span></td>
-                      <td><span data-ng-bind="donation.type"></span></td>
-                      <td><span data-ng-bind="donation._id"></span></td>
-                      <td print-remove>
-                        <a class="btn btn-info btn-flat btn-xs" data-ng-click="$ctrl.viewDonation(donation)"><i class="fa fa-eye"></i> View</a>
-                      </td>
-                    </tr>
-                    <tr data-ng-if="!$ctrl.donor.donations.length">
-                      <td class="text-center" colspan="5">This donor hasn't made any donations yet.</td>
-                    </tr>
-                  </tbody><!-- /.table-body -->
-                </table><!-- /.table -->
-              </div><!-- /.box-body -->
-              <!-- Box footer -->
-              <div class="box-footer">
-                <div class="row">
-                  <div class="col-sm-6 col-md-4 col-lg-2">
-                    <button class="btn btn-default btn-flat btn-block" print-btn print-landscape>
-                      <i class="fa fa-print"></i> Print
-                    </button>
-                  </div>
-                </div>
-              </div><!-- /.box-footer -->
-            </div><!-- /.box -->
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </section><!-- /.content -->
-    `
+    template: '<div id="donor-view"></div>'
   })
   .name;
