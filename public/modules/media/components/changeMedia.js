4f6a8bd1f8ee469bbbf315a5dca0f3724e459aa5
jspaine <painejs@hotmail.com>
2017-04-07 13:03:35 +0200

    media component in react

--- public/modules/media/components/changeMedia.js
+++ public/modules/media/components/changeMedia.js
@@ -1,44 +1,34 @@
+import React from 'react'
+import {Provider} from 'react-redux'
+import ReactDOM from 'react-dom'
+import {AppContainer} from 'react-hot-loader'
 import angular from 'angular';
 
-export default angular.module('media')
+import Media from './Media'
+
+export default angular.module('customer')
   .component('changeMedia', {
-    controller: 'ChangeMediaController',
-    template: `
-      <!-- Change Media Page -->
-      <section class="change-media content">
-        <div class="box box-solid box-primary">
-          <div class="box-header">
-            <h3 class="box-title">CURRENT LOGO</h3>
-          </div><!-- /.box-header-->
-          <!-- Box body -->
-          <div class="box-body">
-            <div class="row">
-              <div class="col-sm-6 col-md-4 col-lg-2">
-                <foodbank-logo store="$ctrl.store" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- Box -->
-        <div class="box box-solid box-primary">
-          <!-- Box header -->
-          <div class="box-header">
-            <h3 class="box-title">CHANGE LOGO</h3>
-          </div><!-- /.box-header-->
-          <!-- Box body -->
-          <div class="box-body">
-            <div class="row">
-              <div class="col-sm-6 col-md-4 col-lg-2">
-                <input type="file" nv-file-select uploader="$ctrl.uploader"/>
-                <div ng-repeat="item in $ctrl.uploader.queue">
-                  <div ng-thumb="{ file:item._file, height:100 }"></div>
-                  <button class="btn btn-success btn-block top-buffer" ng-click="$ctrl.upload(item)">Upload Logo</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    `
+    controller: function($ngRedux) {
+      render(Media)
+
+      function render(Component) {
+        ReactDOM.render(
+          <AppContainer>
+            <Provider store={$ngRedux}>
+              <Component />
+            </Provider>
+          </AppContainer>,
+          document.getElementById('change-media')
+        )
+      }
+
+      if (module.hot) {
+        module.hot.accept('./Media', () => {
+          const Next = require('./Media').default
+          render(Next)
+        })
+      }
+    },
+    template: '<div id="change-media"></div>'
   })
   .name;
--- public/store/media.js
+++ public/store/media.js
@@ -3,7 +3,9 @@ import {CALL_API} from '../middleware/api';
 export const LOAD_REQUEST = 'media/LOAD_REQUEST';
 export const LOAD_SUCCESS = 'media/LOAD_SUCCESS';
 export const LOAD_FAILURE = 'media/LOAD_FAILURE';
-export const SET_MEDIA = 'media/SET_MEDIA';
+export const SAVE_REQUEST = 'media/SAVE_REQUEST';
+export const SAVE_SUCCESS = 'media/SAVE_SUCCESS';
+export const SAVE_FAILURE = 'media/SAVE_FAILURE';
 
 export const loadMedia = () => ({
   [CALL_API]: {
@@ -12,11 +14,29 @@ export const loadMedia = () => ({
   }
 });
 
-export const setMedia = (path, file) => ({
-  type: SET_MEDIA,
-  path,
-  file
-});
+const saveMediaSuccess = response => ({
+  type: SAVE_SUCCESS,
+  response
+})
+
+const saveMediaFailure = error => ({
+  type: SAVE_FAILURE,
+  error
+})
+
+export const saveMedia = file => dispatch => {
+  const formData = new FormData()
+  formData.append('file', file)
+
+  return fetch('/api/media/uploadLogo', {
+    method: 'POST',
+    body: formData
+  })
+    .then(response => response.json().then(json =>
+      dispatch(saveMediaSuccess(json))
+    ))
+    .catch(error => dispatch(saveMediaFailure(error)))
+}
 
 export default (state = {}, action) => {
   switch (action.type) {
@@ -40,14 +60,25 @@ export default (state = {}, action) => {
         fetching: false,
         error: action.error
       };
-    case SET_MEDIA:
+    case SAVE_REQUEST:
       return {
         ...state,
-        data: {
-          ...data,
-          logoPath: action.path,
-          logoFile: action.file
-        }
+        fetching: true,
+        error: null,
+        success: null
+      };
+    case SAVE_SUCCESS:
+      return {
+        ...state,
+        fetching: false,
+        success: true,
+        data: action.response
+      };
+    case SAVE_FAILURE:
+      return {
+        ...state,
+        fetching: false,
+        error: action.error
       };
     default: return state;
   }
