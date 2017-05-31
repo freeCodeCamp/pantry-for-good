d519d408d630551ad14dcc817365374882f399c7
jspaine <painejs@hotmail.com>
2017-05-31 23:10:17 +0200

    add text align, color, font, remove image alignment

--- client/modules/page/components/PageEditor.js
+++ client/modules/page/components/PageEditor.js
@@ -18,10 +18,14 @@ const quillModules = {
     ['bold', 'italic', 'underline','strike', 'blockquote'],
     [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
     ['link', 'image', 'video'],
+    [{'color': []}, {'background': []}],
+    [{'font': []}],
+    [{'size': ['small', false, 'large', 'huge']}],
+    [{'align': []}],
     ['clean']
   ],
   imageDrop: true,
-  imageResize: {}
+  imageResize: {modules: ['Resize', 'DisplaySize']}
 }
 
 const mapStateToProps = (state, ownProps) => ({
