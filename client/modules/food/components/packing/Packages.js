3b7ef66651b568399b6f6b914ec8b7e6c8298a3b
kenjiO <kenjiO@users.noreply.github.com>
2017-07-01 00:50:22 -0700

    Keep a list of packed packages (#145)
    * Keep track of packages when they are packed
    
    * Re-organize react components having to do with packing
    
    * Update api for 'api/packing' endpoint

---
+++ client/modules/food/components/packing/Packages.js
@@ -0,0 +1,72 @@
+/**
+ * Component to display a table of all the packages
+ */
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table'
+import 'react-bootstrap-table/dist/react-bootstrap-table.min.css'
+
+import selectors from '../../../../store/selectors'
+import {listPackages } from '../../reducers/packing'
+
+import {Box, BoxBody, BoxHeader} from '../../../../components/box'
+
+const mapStateToProps = state => ({
+  loading: selectors.food.packing.loading(state),
+  error: selectors.food.packing.loadError(state),
+  packages: selectors.food.packing.packages(state)
+})
+
+const mapDispatchToProps = dispatch => ({
+  load: () => dispatch(listPackages())
+})
+
+class Packages extends Component {
+
+  componentWillMount() {
+    this.props.load()
+  }
+
+  /**
+   * Convert an array of foodItems to a string listing the item names
+   */
+  formatContents = contentList => {
+    return contentList.reduce((prev, curr) => {return `${prev} ${curr.name},`}, "")
+  }
+  
+  render() {
+    const {loading, error, packages} = this.props
+    return (
+      <Box>
+        <BoxHeader heading="Packed Packages" />
+        <BoxBody loading={loading} error={error}>
+          <BootstrapTable
+            data={packages || []}
+            keyField="_id"
+            options={{
+              defaultSortName: "datePacked",
+              defaultSortOrder: 'desc',
+              noDataText: loading ? '' : 'No packages'
+            }}
+            hover
+            striped
+            pagination
+          >
+            <TableHeaderColumn dataField="_id" width="40px" dataSort>_id</TableHeaderColumn>
+            <TableHeaderColumn  dataField="customer" dataFormat={customer => customer.id} width="20px">
+              Customer
+            </TableHeaderColumn>
+            <TableHeaderColumn dataField="datePacked" width="40px">
+              Date Packed
+            </TableHeaderColumn>
+            <TableHeaderColumn  dataField="contents" dataFormat={this.formatContents} width="80px">
+              Contents
+            </TableHeaderColumn>
+          </BootstrapTable>
+        </BoxBody>
+      </Box>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Packages)
--- client/store/entities.js
+++ client/store/entities.js
@@ -7,6 +7,7 @@ export default function entities(state = {
   fields: {},
   foodCategories: {},
   foodItems: {},
+  packages: {},
   pages: {},
   questionnaires: {},
   sections: {},
