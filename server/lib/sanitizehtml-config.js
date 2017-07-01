8ea4e70f1d830621137949699eae38581eb9c471
jspaine <painejs@hotmail.com>
2017-07-01 15:55:00 +0200

    use quill css for ContentPage layout

--- server/lib/sanitizehtml-config.js
+++ server/lib/sanitizehtml-config.js
@@ -1,14 +1,14 @@
 import sanitizeHtml from 'sanitize-html'
 
 export default {
-  allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img', 'span']),
+  allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img', 'span', 'h1', 'h2', 's', 'u']),
   allowedAttributes: {
     img: ['src', 'width', 'style'],
     span: ['style', 'data-id', 'data-label'],
-    a: ['href']
+    a: ['href'],
+    p: ['class']
   },
   allowedClasses: {
-    p: ['ql-align-center', 'ql-align-right', 'ql-align-justify'],
     span: ['ql-placeholder-content']
   }
 }
