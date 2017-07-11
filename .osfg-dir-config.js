508b9378162c56f5a59d74ebc0680a0981752d62
Michael D. Johnson <michael@freecodecamp.com>
2017-07-11 19:17:57 -0400

    Create .osfg-dir-config.js

---
+++ .osfg-dir-config.js
@@ -0,0 +1,29 @@
+/*
+  OPEN SOURCE FOR GOOD DIRECTORY SAMPLE CONFIG FILE 
+
+  title: Displayed at the Top and as the WebPage Title
+  demoVideo: for the moment it only works with a regular YouTube links.
+  liveDemo: address to the Homepage of the Project.
+  description: an ES6 Template String with Github Markdown.
+               Keep it relatively short, a paragraph tops.
+  body: an ES6 Template String with Github Markdown. 
+        Please include the license and FreeCodeCamp copyright at the end.
+
+  If the 'liveDemo' or the 'demoVideo' aren't yet available you can 
+  exclued them, they just won't be added to the Project's page.
+*/
+
+module.exports = {
+  title: 'Pantry for Good',
+  demoVideo: '',
+  liveDemo: 'https://pantry-for-good.herokuapp.com/',
+  description: `An open source food bank logistics and food inventory management tool
+`,
+  body: `
+### License
+
+This computer software is licensed under the Open Source [BSD-3-Clause](https://github.com/freeCodeCamp/Mail-for-Good/blob/master/LICENSE.md).
+
+Copyright (c) 2017, freeCodeCamp.
+`
+};
