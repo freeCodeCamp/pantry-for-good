d1a57120644d5e0874de8e8d3f024887dcd391b0
David Cheung <davycheung@users.noreply.github.com>
2017-09-10 04:21:41 -0400

    user login or redirect for change password and edit profile pages, no need to check roles for those (#237)

--- client/modules/users/UserRouter.js
+++ client/modules/users/UserRouter.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import {values} from 'lodash'
 import {Route} from 'react-router-dom'
 
-import {ADMIN_ROLE, clientRoles} from '../../../common/constants'
+import {ADMIN_ROLE} from '../../../common/constants'
 import UserList from './components/UserList'
 import ChangePassword from './components/ChangePassword'
 import ConfirmNewGoogleAccount from './components/ConfirmNewGoogleAccount'
@@ -14,11 +13,11 @@ import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
 import requireRole from '../../components/router/requireRole'
 import SwitchWithNotFound from '../../components/router/SwitchWithNotFound'
+import userOrRedirect from '../../components/router/userOrRedirect'
 
 import './css/users.css'
 
 const IsAdmin = requireRole([ADMIN_ROLE])
-const IsUser = requireRole([ADMIN_ROLE, ...values(clientRoles)])
 
 const UserRouter = ({match}) =>
   <SwitchWithNotFound>
@@ -35,12 +34,12 @@ const UserRouter = ({match}) =>
     <Route
       path={`${match.url}/change-password`}
       exact
-      component={IsUser(ChangePassword)}
+      component={userOrRedirect(ChangePassword)}
     />
     <Route
       path={`${match.url}/profile`}
       exact
-      component={IsUser(EditProfile)}
+      component={userOrRedirect(EditProfile)}
     />
     <Route
       path={`${match.url}/forgot-password`}
