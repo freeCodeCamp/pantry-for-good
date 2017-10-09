6a923377dc9cc0713761b29721dd28379da0ebe8
icpmacdo <ianmcanada@gmail.com>
2017-10-09 14:15:19 -0400

    changed default startDate to current date

--- server/models/food.js
+++ server/models/food.js
@@ -1,42 +1,43 @@
 import mongoose from 'mongoose'
 
-import {modelTypes} from '../../common/constants'
+import { modelTypes } from '../../common/constants'
 
-const {Schema} = mongoose
+const { Schema } = mongoose
 
 const FoodItemSchema = new Schema({
-  name: {
-    type: String,
-    trim: true
-  },
-  quantity: {
-    type: Number,
-    default: 0
-  },
-  startDate: {
-    type: Date.now
-  },
-  frequency: {
-    type: Number,
-    default: 1
-  },
-  deleted: {
-    type: Boolean,
-    default: false
-  }
+    name: {
+        type: String,
+        trim: true
+    },
+    quantity: {
+        type: Number,
+        default: 0
+    },
+    startDate: {
+        type: Date,
+        default: Date.now
+    },
+    frequency: {
+        type: Number,
+        default: 1
+    },
+    deleted: {
+        type: Boolean,
+        default: false
+    }
 })
 
 const FoodSchema = new Schema({
-  category: {
-    type: String,
-    required: 'Please fill in a category name',
-    trim: true
-  },
-  items: [FoodItemSchema],
-  deleted: {
-    type: Boolean,
-    default: false
-  }
+    category: {
+        type: String,
+        required: 'Please fill in a category name',
+        trim: true
+    },
+    items: [FoodItemSchema],
+    deleted: {
+        type: Boolean,
+        default: false
+    }
 })
 
 export default mongoose.model(modelTypes.FOOD, FoodSchema)
