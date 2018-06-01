6e48973136876f38d14e706728d22ae36af1369c
Kenji Okamoto <kenjiO@users.noreply.github.com>
2018-05-31 17:00:00 -0700

    removed unused path and route props in Header (#379)

--- client/modules/core/components/Header.js
+++ client/modules/core/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import {connect} from 'react-redux'
-import {trim} from 'lodash'
 
 import {ADMIN_ROLE} from '../../../../common/constants'
 import selectors from '../../../store/selectors'
@@ -11,27 +10,20 @@ import AdminNavbar from './navbar/AdminNavbar'
 
 const mapStateToProps = state => ({
   user: selectors.auth.getUser(state),
-  route: state.router.location,
   settings: selectors.settings.getSettings(state)
 })
 
-const Header = ({settings, user, route}) => {
-  const path = trim(route.pathname, '/')
+const Header = ({settings, user}) => {
   const isAdmin = user && user.roles.find(r => r === ADMIN_ROLE)
 
   return isAdmin ?
     <AdminNavbar user={user} settings={settings} /> :
-    <ClientNavbar
-      user={user}
-      settings={settings}
-      path={path}
-    />
+    <ClientNavbar user={user} settings={settings} />
 }
 
 Header.propTypes = {
   settings: PropTypes.object,
   user: PropTypes.object,
-  route: PropTypes.object.isRequired
 }
 
 export default connect(mapStateToProps)(Header)
