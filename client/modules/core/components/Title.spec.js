704064813c6ed347b9723d3e68c1c10a3ffe654c
Kimberlee I. Model <kimee.i.model@gmail.com>
2018-07-05 19:48:18 -0400

    Complete tests for client/modulse/core/components/ Title.js and Footer.js

--- client/modules/core/components/Title.spec.js
+++ client/modules/core/components/Title.spec.js
@@ -12,108 +12,117 @@ describe("Title", function()
 
   it("should display the org as the Title when there is one path element", function()
   {
-    let t = mount(<Title settings={{organization: "FakeOrg"}}
+    let t = mount(<Title settings={{organization: "Fake"}}
       route={{pathname: "fake"}}
       getCustomer={(id) => {return {fullName: names[id]}}}
       getVolunteer={(id) => {return {fullName: names[id]}}}
       getDonor={(id) => {return {fullName: names[id]}}} />)
     expect(t != null && t != undefined).to.eql(true)
+    t.setProps({settings: {organization: "FakeOrg"}})
     t.update()
     expect(document.title).to.equal("FakeOrg")
   })
   
   it("should use path elements and org when there is more than one path element", function()
   {
-    let t = mount(<Title settings={{organization: "FakeOrg"}}
+    let t = mount(<Title settings={{organization: "Fake"}}
       route={{pathname: "two/fake/elements"}}
       getCustomer={(id) => {return {fullName: names[id]}}}
       getVolunteer={(id) => {return {fullName: names[id]}}}
       getDonor={(id) => {return {fullName: names[id]}}} />)
     expect(t != null && t != undefined).to.eql(true)
+    t.setProps({settings: {organization: "FakeOrg"}})
     t.update()
     expect(document.title).to.equal("Elements - Fake - FakeOrg")
   })
   
   it("should use no more than two path elements", function()
   {
-    let t = mount(<Title settings={{organization: "FakeOrg"}}
+    let t = mount(<Title settings={{organization: "Fake"}}
       route={{pathname: "more/than/two/fake/elements"}}
       getCustomer={(id) => {return {fullName: names[id]}}}
       getVolunteer={(id) => {return {fullName: names[id]}}}
       getDonor={(id) => {return {fullName: names[id]}}} />)
     expect(t != null && t != undefined).to.eql(true)
+    t.setProps({settings: {organization: "FakeOrg"}})
     t.update()
     expect(document.title).to.equal("Two - Than - FakeOrg")
   })
   
   it("should use volunteer and org when it is the volunteer list", function()
   {
-    let t = mount(<Title settings={{organization: "FakeOrg"}}
+    let t = mount(<Title settings={{organization: "Fake"}}
       route={{pathname: "fake/volunteers/list"}}
       getCustomer={(id) => {return {fullName: names[id]}}}
       getVolunteer={(id) => {return {fullName: names[id]}}}
       getDonor={(id) => {return {fullName: names[id]}}} />)
     expect(t != null && t != undefined).to.eql(true)
+    t.setProps({settings: {organization: "FakeOrg"}})
     t.update()
     expect(document.title).to.equal("Volunteers - FakeOrg")
   })
   
   it("should use volunteer name when it is a specific volunteer", function()
   {
-    let t = mount(<Title settings={{organization: "FakeOrg"}}
+    let t = mount(<Title settings={{organization: "Fake"}}
       route={{pathname: "fake/volunteers/1"}}
       getCustomer={(id) => {return {fullName: names[id]}}}
       getVolunteer={(id) => {return {fullName: names[id]}}}
       getDonor={(id) => {return {fullName: names[id]}}} />)
     expect(t != null && t != undefined).to.eql(true)
+    t.setProps({settings: {organization: "FakeOrg"}})
     t.update()
     expect(document.title).to.equal("Fake Volunteer - Volunteers - FakeOrg")
   })
   
   it("should use customer and org when it is the customer list", function()
   {
-    let t = mount(<Title settings={{organization: "FakeOrg"}}
+    let t = mount(<Title settings={{organization: "Fake"}}
       route={{pathname: "fake/customers/list"}}
       getCustomer={(id) => {return {fullName: names[id]}}}
       getVolunteer={(id) => {return {fullName: names[id]}}}
       getDonor={(id) => {return {fullName: names[id]}}} />)
     expect(t != null && t != undefined).to.eql(true)
+    t.setProps({settings: {organization: "FakeOrg"}})
     t.update()
     expect(document.title).to.equal("Customers - FakeOrg")
   })
   
   it("should use customer name when it is a specific customer", function()
   {
-    let t = mount(<Title settings={{organization: "FakeOrg"}}
+    let t = mount(<Title settings={{organization: "Fake"}}
       route={{pathname: "fake/customers/2"}}
       getCustomer={(id) => {return {fullName: names[id]}}}
       getVolunteer={(id) => {return {fullName: names[id]}}}
       getDonor={(id) => {return {fullName: names[id]}}} />)
     expect(t != null && t != undefined).to.eql(true)
+    t.setProps({settings: {organization: "FakeOrg"}})
     t.update()
     expect(document.title).to.equal("Fake Customer - Customers - FakeOrg")
   })
   
   it("should use donor and org when it is the donor list", function()
   {
-    let t = mount(<Title settings={{organization: "FakeOrg"}}
+    let t = mount(<Title settings={{organization: "Fake"}}
       route={{pathname: "fake/donors/list"}}
       getCustomer={(id) => {return {fullName: names[id]}}}
       getVolunteer={(id) => {return {fullName: names[id]}}}
       getDonor={(id) => {return {fullName: names[id]}}} />)
     expect(t != null && t != undefined).to.eql(true)
+    t.setProps({settings: {organization: "FakeOrg"}})
     t.update()
     expect(document.title).to.equal("Donors - FakeOrg")
   })
   
   it("should use donor name when it is a specific donor", function()
   {
-    let t = mount(<Title settings={{organization: "FakeOrg"}}
+    let t = mount(<Title settings={{organization: "Fake"}}
       route={{pathname: "fake/donors/3"}}
       getCustomer={(id) => {return {fullName: names[id]}}}
       getVolunteer={(id) => {return {fullName: names[id]}}}
       getDonor={(id) => {return {fullName: names[id]}}} />)
     expect(t != null && t != undefined).to.eql(true)
+    t.setProps({settings: {organization: "FakeOrg"}})
     t.update()
     expect(document.title).to.equal("Fake Donor - Donors - FakeOrg")
   })
